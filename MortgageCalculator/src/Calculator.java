bf5af98ed20edde245177ce5e53755ae5cfa62c7
Vijay <vijayaddepalli@outlook.com>
2022-11-16 21:59:02 +0530

    Currency Icon Issue Resolved

--- MortgageCalculator/src/Calculator.java
+++ MortgageCalculator/src/Calculator.java
@@ -1,8 +1,11 @@
 import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 /*
  * 
  * Mortgage Calculator
+ * 
+ * M=P((r(1+r)^n)/((1+r)^n)-1)
  * 
  */
 public class Calculator {
@@ -20,7 +23,7 @@ public class Calculator {
 		double noOfMonths = years*12;
 		double mortgageAmount = principalAmount*(monthlyInterestRate*(Math.pow((1+monthlyInterestRate), noOfMonths))/(Math.pow(1+monthlyInterestRate, noOfMonths)-1));
 		System.out.println("Mortgage: $"+String.format("%.2f", mortgageAmount));
-		NumberFormat mortgageFormat = NumberFormat.getCurrencyInstance();
+		NumberFormat mortgageFormat = NumberFormat.getCurrencyInstance(Locale.US);
 		System.out.println("Mortgage: "+mortgageFormat.format(mortgageAmount));
 
 	}
