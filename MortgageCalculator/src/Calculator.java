46f430e7ebf7ef55852cbfe4f45fb8989febadbb
Vijay <vijayaddepalli@outlook.com>
2022-11-18 08:34:24 +0530

    Validations Added

--- MortgageCalculator/src/Calculator.java
+++ MortgageCalculator/src/Calculator.java
@@ -1,6 +1,7 @@
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
+
 /*
  * 
  * Mortgage Calculator
@@ -11,20 +12,44 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Principal:");
-		double principalAmount = sc.nextDouble();
-		System.out.print("Annual Interest Rate:");
-		double annualInterestRate = sc.nextDouble();
-		System.out.print("Period(years):");
-		int years = sc.nextInt();
-		double monthlyInterestRate = (annualInterestRate/100)/12;
-		double noOfMonths = years*12;
-		double mortgageAmount = principalAmount*(monthlyInterestRate*(Math.pow((1+monthlyInterestRate), noOfMonths))/(Math.pow(1+monthlyInterestRate, noOfMonths)-1));
-		System.out.println("Mortgage: $"+String.format("%.2f", mortgageAmount));
+		double principalAmount = 0;
+		double annualInterestRate = 0;
+		int years = 0;
+
+		while (true) {
+			System.out.print("Principal(1k-100K): ");
+			principalAmount = sc.nextDouble();
+			if (1000 <= principalAmount && 1000000 >= principalAmount)
+				break;
+			System.out.println("Enter Amount between 1,000 to 1,000,000");
+		}
+
+		while (true) {
+			System.out.print("Annual Interest Rate:");
+			annualInterestRate = sc.nextDouble();
+			if (1 <= annualInterestRate && 30 >= annualInterestRate)
+				break;
+			System.out.println("Enter value between 1 to 30");
+
+		}
+
+		while (true) {
+			System.out.print("Period(years):");
+			years = sc.nextInt();
+			if (1 <= years && 30 >= years)
+				break;
+			System.out.println("Enter value between 1 to 30");
+		}
+
+		double monthlyInterestRate = (annualInterestRate / 100) / 12;
+		double noOfMonths = years * 12;
+		double mortgageAmount = principalAmount
+				* (monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), noOfMonths))
+						/ (Math.pow(1 + monthlyInterestRate, noOfMonths) - 1));
 		NumberFormat mortgageFormat = NumberFormat.getCurrencyInstance(Locale.US);
-		System.out.println("Mortgage: "+mortgageFormat.format(mortgageAmount));
+		System.out.println("Mortgage: " + mortgageFormat.format(mortgageAmount));
 
 	}
 
