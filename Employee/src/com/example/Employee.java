f207159bd543a8ffed50426fe4617bf12bb57ec9
Vijay <vijayaddepalli@outlook.com>
2022-11-24 08:34:16 +0530

    constructor added and static member added

--- Employee/src/com/example/Employee.java
+++ Employee/src/com/example/Employee.java
@@ -4,12 +4,29 @@ public class Employee {
 	
 	private int baseSalary;
 	private int hourlyRate;
+	private static int noOfEmployees; //static member creation
 	
+	//constructor overloading
+	public Employee(int baseSalary) {
+		this(baseSalary, 0);
+	}
+	
+	public Employee(int baseSalary, int hourlyRate) {
+		setBaseSalary(baseSalary);
+		setHourlyRate(hourlyRate);
+		noOfEmployees++;
+	}
+	
+	//static method
+	public static void printNoOfEmployees() {
+		System.out.println("No of Employees: "+noOfEmployees);
+	}
+
 	public int getBaseSalary() {
 		return baseSalary;
 	}
 	
-	public void setBaseSalary(int baseSalary) {
+	private void setBaseSalary(int baseSalary) {
 		if(baseSalary<=0)
 			throw new IllegalArgumentException("base salary can not be 0 or less");
 		this.baseSalary = baseSalary;
@@ -19,8 +36,8 @@ public class Employee {
 		return hourlyRate;
 	}
 	
-	public void setHourlyRate(int hourlyRate) {
-		if(hourlyRate<=0)
+	private void setHourlyRate(int hourlyRate) {
+		if(hourlyRate<0)
 			throw new IllegalArgumentException("hourly rate can not be 0 or less");
 		this.hourlyRate = hourlyRate;
 	}
--- Employee/src/com/example/Main.java
+++ Employee/src/com/example/Main.java
@@ -4,11 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Employee employee = new Employee();
-		employee.setBaseSalary(30000);
-		employee.setHourlyRate(20);
+		Employee employee = new Employee(50_000, 20);
+		Employee emp = new Employee(35_000); //with 0 hourly rate
+		Employee.printNoOfEmployees(); //calling static member by class name
 		int wage = employee.calculateWage(5);
 		System.out.println(wage);
+		System.out.print(emp.calculateWage(5));
  
 	}
 	
